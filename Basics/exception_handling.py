149c2c73862b32bd7d0a41c3801f0acc1c9f57c8
Achyu-t <acchyutbhattarai@gmail.com>
2026-06-02 18:22:27 +0545

    OOP basics

---
+++ Basics/exception_handling.py
@@ -0,0 +1,23 @@
+# Exception : Event that interrupts flow of a program. There are many different types of exception. 
+
+
+try:
+    number =int( input("Enter a number: "))
+
+    print(1/number)
+
+except ZeroDivisionError :
+    print("You cant divide by 0 IDIOT !!!!")
+
+except ValueError :
+    print("Enter a number you dumbass")
+
+
+# Another way to do is  :     except Exception :
+#                                 print("Something went wrong")
+                               
+# This is a bad practice because it catches all exceptions. It is best to give user idea of what actual error it is . Genrally used as a last resort.
+
+
+finally :                                           # Always executed regardless of exception or not.
+    print("Do some cleanup here")
