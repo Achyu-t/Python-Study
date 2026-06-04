6237092f9f113c10aabfd2c59df737516f7a3324
Achyu-t <acchyutbhattarai@gmail.com>
2026-06-04 16:17:58 +0545

    Upto GUI

--- Basics/ipop.py
+++ Basics/ipop.py
@@ -836,12 +836,33 @@ False
 #----------------------------------------------------------------------------------------------------------------------------------------------#
 
 
+# API
+
+import requests
 
 
+base_url = 'https://pokeapi.co/api/v2/'
+
+def get_pokemon_info(name):
+    url = f"{base_url}/pokemon/{name}"
+    response = requests.get(url)
+    print(response)
+
+    if response.status_code ==200:
+        pokemon_data = response.json()
+        return pokemon_data
+
+    else:
+        print(f"Failed to retreive data {response.status_code}")
+
+pokemon_name = "goodra"
+
+pokemon_info = get_pokemon_info(pokemon_name)
 
 
-
-
+if pokemon_info:
+    print(f"{pokemon_info['name'].capitalize()}")
+    print(f"{pokemon_info['height']}")
 
 
 
